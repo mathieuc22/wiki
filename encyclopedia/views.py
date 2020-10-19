c5bba889634ecfa8b7e49be9482798cf2e2c8efc
Mathieu <mathieu.collet@gmail.com>
2020-10-19 18:54:20 +0200

    back to the course, add entries url and pages, convert md to html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from markdown2 import Markdown
 
 from . import util
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def new(request):
+    return render(request, "encyclopedia/new.html", {
+    })
+
+def entry(request, entry):
+    return render(request, f"encyclopedia/entry.html", {
+        "entry": markdowner.convert(util.get_entry(entry))
+    })
+    #return HttpResponse(markdowner.convert(util.get_entry(entry)))
