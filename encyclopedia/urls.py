3b5e71bdcbb75082a54943f988be254b386dd364
Mathieu <mathieu.collet@gmail.com>
2020-10-23 18:14:03 +0200

    new 404 page and search implementation

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("new", views.new, name="new"),
-    path("wiki/<str:entry>", views.entry, name="entry")
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseRedirect
 from markdown2 import Markdown
 from django.http import Http404
 
@@ -19,8 +19,25 @@ def new(request):
 def entry(request, entry):
     e = util.get_entry(entry)
     if  e is None:
-        return HttpResponseNotFound('<h1>Page not found</h1>')
+        return render(request, "encyclopedia/404.html", {
+        })
     else:
         return render(request, f"encyclopedia/entry.html", {
             "entry": markdowner.convert(e)
         })
+
+def search(request):
+    if request.method == "POST":
+        q = request.POST['q']
+        match = util.get_entry(q)
+        if  match is None:
+            matching = [s for s in util.list_entries() if q.lower() in s.lower()]
+            return render(request, "encyclopedia/search.html", {
+                "entries": matching
+            })
+        else:
+            return render(request, f"encyclopedia/entry.html", {
+                "entry": markdowner.convert(match)
+            })
+    else:
+        return HttpResponseRedirect("/")
